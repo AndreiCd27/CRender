f1e63e836815ec40d330684b18f9823bff477ea2
Andrei27 <andreic271113@gmail.com>
2026-03-30 16:35:16 +0300

    added opengl_linux_install.sh

---
+++ .github/linuxinstall.sh
@@ -0,0 +1,19 @@
+!#bin/bash
+
+# See Linux OpenGL & GLFW dependencies
+# https://medium.com/geekculture/a-beginners-guide-to-setup-opengl-in-linux-debian-2bfe02ccd1e
+
+# OpenGL
+sudo apt-get install cmake pkg-config
+sudo apt-get install mesa-utils libglu1-mesa-dev freeglut3-dev mesa-common-dev
+sudo apt-get install libglew-dev libglfw3-dev libglm-dev
+sudo apt-get install libao-dev libmpg123-dev
+
+# GLFW
+cd /usr/local/lib/
+git clone https://github.com/glfw/glfw.git
+cd glfw
+cmake .
+make
+sudo make install
+
