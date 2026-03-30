f163d3ba1b5f8e877dfd8e95b9faa20b6e9862ef
Andrei27 <andreic271113@gmail.com>
2026-03-30 17:40:03 +0300

    linux install fix 2

--- opengl_linux_install.sh
+++ opengl_linux_install.sh
@@ -16,9 +16,9 @@ sudo apt install libwayland-bin libwayland-dev
 sudo apt install libxkbcommon-dev
 
 #install xinerama for cmake
-sudo apt install libxinerama-dev (if not installed already)
+sudo apt install libxinerama-dev #(if not installed already)
 #install xcursor for cmake
-sudo apt install libxcursor-dev (if not installed already)
+sudo apt install libxcursor-dev #(if not installed already)
 
 # GLFW
 cd /usr/local/lib/
